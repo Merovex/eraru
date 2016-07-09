6712c99de3c00c694bba22910e8efcb5837c4022
Merovex <dausha@gmail.com>
2016-07-09 15:50:06 -0400

    Hasty Commit: 2016-07-09 15:50:05

--- app/components/version/version-directive_test.js
+++ app/components/version/version-directive_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-describe('myApp.version module', function() {
-  beforeEach(module('myApp.version'));
+describe('korektuApp.version module', function() {
+  beforeEach(module('korektuApp.version'));
 
   describe('app-version directive', function() {
     it('should print current version', function() {
