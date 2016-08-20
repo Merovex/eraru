a7b78c6e4af4fad290f2e2dab47887abecc02162
Merovex <dausha@gmail.com>
2016-08-20 11:13:21 -0400

    Making it less custom.

--- app/controllers/fix_controller.rb
+++ app/controllers/fix_controller.rb
@@ -2,31 +2,14 @@ class FixController < ApplicationController
 	protect_from_forgery except: :create_github_issue
 	def create_github_issue
 		require 'octokit'
-	    require 'awesome_print'
-		# TYPO , SYNTAX , IDEA, IMPLAUSIBLE, CONFUSION , DULL
-
-	    repo_name = 'postal-marine-series.verki'
-	    repo_name = 'Konspiro'
-	    user_name = 'Merovex'
-
-	    format = (params[:pbk] == 'on') ? 'pbk' : 'mobi'
-
-	    issue = {
-	      # params: params,
-	      title: "#{params[:kind]} in #{params[:book]} at #{format[0]}.#{params[:location]}",
-	      labels: [
-	        params[:kind].downcase
-	      ]
-	    }
-	    labels = [params[:kind].downcase, 'reader']
-	    name = (params[:name] != '') ? params[:name] : 'Anonymous'
-	    reported_by = "Reported by: #{name} (#{params[:email]})".gsub('()',"")
-
-		human = check_recaptcha()
-
-	    text =<<-EOT
+		if ishuman?()
+		    format 		= (params[:pbk] == 'on') ? 'pbk' : 'mobi'
+		    title 		= "#{params[:kind]} in #{params[:book]} at #{format[0]}.#{params[:location]}"
+		    name 		= (params[:name] != '') ? params[:name] : 'Anonymous'
+		    email       = "(#{params[:email]})".gsub('()',"")
+		    text        =<<-EOT
 ## Contributor
-* #{reported_by}
+* Reported by: #{name} #{email}
 
 ## Metadata
 * Book: #{params[:book]}
@@ -38,25 +21,21 @@ class FixController < ApplicationController
 #{params[:text].gsub(/^#/, '###')}
 EOT
 
-		if human
 		    github = Octokit::Client.new(:access_token => ENV['github_secret'])
-		    repo = "#{user_name}/#{params[:repo]}"
-		    res = github.create_issue(repo, issue[:title], text, {labels: labels})
+		    res    = github.create_issue(params[:repo], title, text, {labels: [params[:kind].downcase, 'reader']})
 		end
-	    redirect_to "http://dausha.net/korektu/thanks?b=#{params[:book]}"
-	    # ap res
+	    redirect_to "#{ENV['thanks_url']}?b=#{params[:book]}"
 	end
 	private
-		def check_recaptcha
+		def ishuman?
+            secret_key = ENV['recaptcha_secret']
+            return true if secret_key.nil? # You didn't set up Recaptcha, you get what you get.
+
 			recaptcha_url = "https://www.google.com/recaptcha/api/siteverify"
-			secret_key = ENV['recaptcha_secret']
-			response = params["g-recaptcha-response"]
+			data = "-d \"secret=#{secret_key}&response=#{params["g-recaptcha-response"]}\""
 
-			data = "-d \"secret=#{secret_key}&response=#{response}\""
-
-			status = `curl --request POST #{data} #{recaptcha_url}`
-			hash = JSON.parse(status)
+			status   = `curl --request POST #{data} #{recaptcha_url}`
+			hash     = JSON.parse(status)
 			hash["success"] == true ? true : false
-			# raise "Recaptcha! #{pass} #{status.inspect}"
 		end
 end
