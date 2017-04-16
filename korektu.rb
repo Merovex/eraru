02a0a5eafcce151d04dbcee956e5a1e3ceb9deea
Merovex <dausha@gmail.com>
2017-04-16 15:07:40 -0400

    Hasty Commit: 2017-04-16 15:07:35

--- korektu.rb
+++ korektu.rb
@@ -35,7 +35,7 @@ EOT
     end
   end
   def trusted_origin_url?()
-    http_origin = request.env['HTTP_ORIGIN'].sub("http://").sub("https://").sub(":4000")
+    http_origin = request.env['HTTP_ORIGIN'].sub("http://","").sub("https://","").sub(":4000","")
     good = ENV['origin_url'].split(';').include?(http_origin)
     puts "Trusted? #{good} (#{ENV['origin_url'].split(';').inspect}) (#{request.env['HTTP_ORIGIN']})"
     return good
